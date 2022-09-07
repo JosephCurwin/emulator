54cb2ba58a980b13033783cf9cfdd0c001a91a6e
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-09-07 15:56:58 +0200

    added log module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestEmulatorModule } from './rest-emulator/rest-emulator.module';
 import { ServerSentEventModule } from './server-sent-event/server-sent-event.module';
+import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [RestEmulatorModule, ServerSentEventModule],
+  imports: [RestEmulatorModule, ServerSentEventModule, LogModule],
   controllers: [],
   providers: [],
 })
