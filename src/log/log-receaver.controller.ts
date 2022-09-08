e55d9ea3d1ae8fad48442cbdcccea15936941793
Erwin Feld <feld@dresearch-fe.de>
2022-09-08 15:48:53 +0200

    removed unesed code

--- src/log/log-receaver.controller.ts
+++ src/log/log-receaver.controller.ts
@@ -3,29 +3,43 @@ import * as fs from 'fs'
 // import { appendFileSync } from 'fs';
 
 
+// create a interface for x to be used in the post method
+export interface Dto {
+    log: {
+        apiRoute: string,
+        timestamp: string,
+        streamOutput: string
+    }
+}
+
+
+
 @Controller('log-receaver')
 export class LogReceaverController {
 
 /*
 * method to receaves data from client and console.log the receaved data
 *  */
-
+/* TODO create a new file with name for log file  */
 @Post()
 addLog(
-    @Body() createTaskDto: any) {
-        // write the createTaskDto to a local json file
-    console.log(createTaskDto);
-    // write createTaskDto to a local json file
-        // write the createTaskDto to a local json file seperated by a new line and a comma and adding a timestamp prperty
+    @Body() dto: Dto) {
+        // 
+        // check the value of the apiRoute and create a new file with the name of the apiRoute if it does not exist
+        //
+        fs.appendFileSync(`src/log/${dto.log.apiRoute}.json`, JSON.stringify(dto) + ',\r');
 
-            /* TODO make proper JSON FIlE  */
-        fs.appendFileSync('src/log/log-receaver.json', JSON.stringify(createTaskDto) + ',\r \n' + 'timestamp: ' + Date.now() + ',\r \n');
+        // check if the file exist
+        // if (fs.existsSync(`src/log/${dto.log.apiRoute}.json`)) {   
+        //     // if the file exist append the data to the file
+        //     fs.appendFileSync(`./${dto.log.apiRoute}.json`, JSON.stringify(dto) + ',\r');
+        // } else {
+        //     // if the file does not exist create a new file and append the data to the file
+        //     fs.appendFileSync(`src/log/${dto.log.apiRoute}.json`, JSON.stringify(dto) + ',\r');
+        // }
+        // console.log the data
 
-    // fs.appendFileSync('./log.json', createTaskDto);
-    // fs.writeFileSync('src/log/log-receaver.json', JSON.stringify(createTaskDto.prop));
-    // fs.write('log.json', createTaskDto);
-    // fs.writeFile('log.json', JSON.stringify(createTaskDto));
-    // fs.writeFileSync('./log.json', JSON.stringify(createTaskDto));
+        // fs.appendFileSync('src/log/log-receaver.json', JSON.stringify(dto) + ',\r');
 }
 
     
